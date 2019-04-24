6f1f88dd1e73876ac5518a33e44b2fe04438276b
Leema M <Leema M@Leema>
2019-04-24 12:23:32 +0530

    Commit

---
+++ Maven1/src/test/java/ChromeLaunch.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeLaunch {
+
+		public static void main(String[] args) {
+			
+			ChromeDriver driver=new ChromeDriver();
+			driver.manage().window().maximize();
+}
+}
